f993aa531dd49334aadd5f0dbdf69779be03c8fa
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-11-13 18:37:09 -0300

    feat: add content to login page

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
 
+
 export default function App() {
   return (
     <NavigationContainer>
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,90 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin() {
+    if (email === "" || password === "") {
+      return;
+    }
+  }
+
   return (
-    <View>
-      <Text>Tela de Login</Text>
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require("../../assets/logo.png")} />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Digite o seu email..."
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          placeholder="Digite a sua senha..."
+          style={styles.input}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Fazer Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    objectFit: "contain",
+  },
+  inputContainer: {
+    width: "95%",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 32,
+    paddingHorizontal: 14,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    width: 300,
+    borderRadius: 10,
+    backgroundColor: "#f1f1f1",
+  },
+  button: {
+    width: "95%",
+    height: 40,
+    backgroundColor: "#F76707",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
